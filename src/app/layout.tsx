b5af59736f9b1698c932d81eab1f5c76f11f9309
Aadityasiva <aadityasiva.s@gmail.com>
2025-10-29 00:40:45 +0530

    updated page background colour

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased bg-[#070707]`}>{children}</body>
     </html>
   );
 }
